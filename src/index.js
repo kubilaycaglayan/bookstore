2999d051f1df51c6d17fa8da87dba56be40f150f
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-14 15:35:03 +0530

    Add App component to src/index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <div />,
+  <App />,
   document.getElementById('root'),
 );
