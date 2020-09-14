45c2eb896658aba5c2654fea53ce0f58320af370
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-14 14:44:20 +0300

    Create BooksList component

---
+++ src/components/BooksList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+const mapStateToProps = state => ({
+  books: state.books,
+});
+
+const mapDispatchToProps = () => ({
+
+});
+
+const BooksList = props => {
+  const { books } = props;
+
+  return (
+    <table>
+      {
+        books.map(book => <Book key={book.id} book={book} />)
+      }
+    </table>
+  );
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(BooksList);
