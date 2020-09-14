5cbbace328983b3d1f6bebcda2813518da7ca281
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-14 14:56:45 +0300

    Create Book component

---
+++ src/components/Book.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({ book }) => {
+  const { id, title, category } = book;
+
+  return (
+    <tr>
+      <td>
+        {id}
+      </td>
+      <td>
+        {title}
+      </td>
+      <td>
+        {category}
+      </td>
+    </tr>
+  );
+};
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Book;
