b905154f396eb96b7fc71e022bf71c605465af15
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-15 12:15:49 +0300

    Create BooksForm state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,20 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { categories } from '../constants';
 
-const BooksForm = () => (
-  <form>
-    <input />
-    <select>
-      {
-          categories.map((category, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <option key={index} value={category}>{category}</option>
-          ))
-        }
-    </select>
-    <button type="button">Add Book</button>
-  </form>
-);
+const BooksForm = () => {
+  let state = {
+    title: '',
+    category: '',
+  };
+
+  const setState = newState => {
+    state = {
+      ...state,
+      ...newState,
+    };
+  };
+
+  const handleInputChange = event => {
+    setState({
+      title: event.target.value,
+    });
+  };
+
+  const handleSelectChange = event => {
+    setState({
+      category: event.target.value,
+    });
+  };
+
+  return (
+    <form>
+      <input onChange={handleInputChange} />
+      <select onChange={handleSelectChange}>
+        {
+            categories.map((category, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <option key={index} value={category}>{category}</option>
+            ))
+          }
+      </select>
+      <button type="button">Add Book</button>
+    </form>
+  );
+};
 
 export default connect(
   null,
