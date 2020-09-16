4298cd9ca3314e01a0c08f4695d3b4b82846c4ba
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-16 16:28:22 +0530

    Create navbar component (incomplete)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import NavBar from './NavBar';
 
 const App = () => (
   <div>
+    <NavBar />
     <BooksList />
     <BooksForm />
   </div>
