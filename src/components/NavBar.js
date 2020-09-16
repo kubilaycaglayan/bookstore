ec533184942ae5caaa3a2230d92f85822d969190
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-16 15:43:43 +0300

    Add Navbar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 const NavBar = () => (
   <nav>
-    
+    <div className="Bookstore-CMS">
+      Bookstore CMS
+    </div>
+    <div className="BOOKS">
+      BOOKS
+    </div>
+    <div className="CATEGORIES">
+      CATEGORIES
+    </div>
+    <div className="Oval" />
   </nav>
 );
+
+export default NavBar;
